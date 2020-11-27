5525ae952e75ad532ad905aab5abf637a16d85c2
unknown <ham_int@hotmail.com>
2020-11-27 21:28:10 +0700

    debuging

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(bodyparser.json());
 app.use(cors())
 
-app.get('/',(req,res)=>{res.send(database.user);})
+app.get('/',(req,res)=>{res.send('server is running');})
 
 app.post('/signin',(req,res)=>{signin.handleSignin(req,res,db,bcrypt)})
 
