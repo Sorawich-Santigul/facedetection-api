8f8e743e2b4ef0f102fc69545c26f3aae4358747
unknown <ham_int@hotmail.com>
2020-11-27 21:32:32 +0700

    changing port

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
 
 app.post('/imageurl',(req,res)=>{image.handleApicall(req,res)})
 
-app.listen(process.env.PORT || 3000,()=>{
+app.listen(process.env.PORT || 3001,()=>{
 	console.log('app is running');
 })
